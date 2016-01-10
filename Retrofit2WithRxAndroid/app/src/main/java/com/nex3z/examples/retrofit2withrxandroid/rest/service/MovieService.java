d17fc17dfb9236d0209cc95cbe499c2a04049ceb
nex3z <litianxing9@gmail.com>
2016-01-10 12:12:19 +0800

    Add Retrofit2WithRxAndroid.

---
+++ Retrofit2WithRxAndroid/app/src/main/java/com/nex3z/examples/retrofit2withrxandroid/rest/service/MovieService.java
@@ -0,0 +1,17 @@
+package com.nex3z.examples.retrofit2withrxandroid.rest.service;
+
+import com.nex3z.examples.retrofit2withrxandroid.rest.model.MovieResponse;
+
+import retrofit.http.GET;
+import retrofit.http.Query;
+import rx.Observable;
+
+public interface MovieService {
+
+    String SORT_BY_POPULARITY_DESC = "popularity.desc";
+    String SORT_BY_VOTE_AVERAGE_DESC = "vote_average.desc";
+    String SORT_BY_VOTE_COUNT_DESC = "vote_count.desc";
+
+    @GET("/3/discover/movie")
+    Observable<MovieResponse> getMovies(@Query("sort_by") String sortBy);
+}
