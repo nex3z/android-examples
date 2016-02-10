6e297a28205da14cc94def9f702ed6ea0acc61c5
nex3z <litianxing9@gmail.com>
2016-02-10 14:55:23 +0800

    Add Barcode.

---
+++ Barcode/app/src/main/java/com/nex3z/examples/barcode/MainActivity.java
@@ -0,0 +1,47 @@
+package com.nex3z.examples.barcode;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.zxing.Result;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class MainActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+
+    private ZXingScannerView mScannerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mScannerView = new ZXingScannerView(this);
+
+        setContentView(mScannerView);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mScannerView.setResultHandler(this);
+        mScannerView.startCamera();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mScannerView.stopCamera();
+    }
+
+    @Override
+    public void handleResult(Result result) {
+        Log.v(LOG_TAG, result.getText() + ", " + result.getBarcodeFormat().toString());
+
+        Toast.makeText(this, result.getText(), Toast.LENGTH_LONG).show();
+
+        mScannerView.resumeCameraPreview(this);
+    }
+}
