103cf58ea28fb3af2e0dff142ca12b90f0939255
nex3z <litianxing9@gmail.com>
2016-04-06 19:52:11 +0800

    Fix missing files.

---
+++ CustomSearchSuggestionItem/README.md
@@ -0,0 +1,5 @@
+# Custom Search Suggestion Item
+
+## Description
+
+A simple example of using custom suggestion item in recent query suggestion list.
---
+++ CustomSearchSuggestionItem/app/src/main/java/com/nex3z/examples/customsearchsuggestionitem/MainActivity.java
@@ -0,0 +1,113 @@
+package com.nex3z.examples.customsearchsuggestionitem;
+
+import android.app.SearchManager;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.SearchRecentSuggestions;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+
+    private SearchSuggestionAdapter mSuggestionAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        handleIntent(getIntent());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_search, menu);
+
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        setupSearchView(searchItem);
+
+        return true;
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        Log.v(LOG_TAG, "onNewIntent()");
+        handleIntent(intent);
+    }
+
+    private void handleIntent(Intent intent) {
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            Log.v(LOG_TAG, "handleIntent(): query = " + query);
+
+            SearchRecentSuggestions suggestions = new SearchRecentSuggestions(this,
+                    ScheduleSuggestionProvider.AUTHORITY, ScheduleSuggestionProvider.MODE);
+            suggestions.saveRecentQuery(query, null);
+        }
+    }
+
+    private void setupSearchView(MenuItem searchItem) {
+        SearchManager searchManager =
+                (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        final SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+
+        mSuggestionAdapter = new SearchSuggestionAdapter(this, null, 0);
+        searchView.setSuggestionsAdapter(mSuggestionAdapter);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                Cursor cursor = getRecentSuggestions(newText, 10);
+                mSuggestionAdapter.swapCursor(cursor);
+                return false;
+            }
+        });
+
+        searchView.setOnSuggestionListener(new SearchView.OnSuggestionListener() {
+            @Override
+            public boolean onSuggestionSelect(int position) {
+                return false;
+            }
+
+            @Override
+            public boolean onSuggestionClick(int position) {
+                searchView.setQuery(mSuggestionAdapter.getSuggestionText(position), true);
+                return true;
+            }
+        });
+    }
+
+    public Cursor getRecentSuggestions(String query, int limit) {
+        Uri.Builder uriBuilder = new Uri.Builder()
+                .scheme(ContentResolver.SCHEME_CONTENT)
+                .authority(ScheduleSuggestionProvider.AUTHORITY);
+
+        uriBuilder.appendPath(SearchManager.SUGGEST_URI_PATH_QUERY);
+
+        String selection = " ?";
+        String[] selArgs = new String[] { query };
+
+        if (limit > 0) {
+            uriBuilder.appendQueryParameter(
+                    SearchManager.SUGGEST_PARAMETER_LIMIT, String.valueOf(limit));
+        }
+
+        Uri uri = uriBuilder.build();
+
+        return getContentResolver().query(uri, null, selection, selArgs, null);
+    }
+}
