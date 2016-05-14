126f6248b9ceb6b69f400517c684dd4c8c690451
nex3z <litianxing9@gmail.com>
2016-05-14 19:19:21 +0800

    Add VerticalScrollView.

---
+++ SimpleCustomView/app/src/main/java/com/nex3z/examples/simplecustomview/VerticalScrollView.java
@@ -0,0 +1,184 @@
+package com.nex3z.examples.simplecustomview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.VelocityTracker;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Scroller;
+
+public class VerticalScrollView extends ViewGroup {
+    private static final String LOG_TAG = VerticalScrollView.class.getSimpleName();
+
+    private int mChildrenSize;
+    private int mChildWidth;
+    private int mChildHeight;
+    private int mChildIndex;
+
+    private int mLastX = 0;
+    private int mLastY = 0;
+    private int mLastXIntercept = 0;
+    private int mLastYIntercept = 0;
+
+    private Scroller mScroller;
+    private VelocityTracker mVelocityTracker;
+
+    public VerticalScrollView(Context context) {
+        super(context);
+        init();
+    }
+
+    public VerticalScrollView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public VerticalScrollView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init();
+    }
+
+    private void init() {
+        if (mScroller == null) {
+            mScroller = new Scroller(getContext());
+            mVelocityTracker = VelocityTracker.obtain();
+        }
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        boolean intercepted = false;
+        int x = (int) ev.getX();
+        int y = (int) ev.getY();
+
+        switch (ev.getAction()) {
+            case MotionEvent.ACTION_DOWN: {
+                intercepted = false;
+                if (!mScroller.isFinished()) {
+                    mScroller.abortAnimation();
+                    intercepted = true;
+                }
+                break;
+            }
+            case MotionEvent.ACTION_MOVE: {
+                int dx = x - mLastXIntercept;
+                int dy = y - mLastYIntercept;
+                intercepted = Math.abs(dy) > Math.abs(dx);
+                break;
+            }
+            case MotionEvent.ACTION_UP: {
+                intercepted = false;
+                break;
+            }
+            default: {
+                break;
+            }
+        }
+
+        Log.v(LOG_TAG, "onInterceptTouchEvent(): intercepted = " + intercepted);
+        mLastX = x;
+        mLastY = y;
+        mLastXIntercept = x;
+        mLastYIntercept = y;
+
+        return intercepted;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        mVelocityTracker.addMovement(ev);
+        int x = (int) ev.getX();
+        int y = (int) ev.getY();
+
+        switch (ev.getAction()) {
+            case MotionEvent.ACTION_DOWN: {
+                if (!mScroller.isFinished()) {
+                    mScroller.abortAnimation();;
+                }
+                break;
+            }
+            case MotionEvent.ACTION_MOVE: {
+                int dx = x - mLastX;
+                int dy = y - mLastY;
+                scrollBy(0, -dy);
+                break;
+            }
+            case MotionEvent.ACTION_UP: {
+                break;
+            }
+            default: {
+                break;
+            }
+        }
+        mLastX = x;
+        mLastY = y;
+
+        return true;
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+        int measuredWidth = 0;
+        int measuredHeight = 0;
+        final int childCount = getChildCount();
+        measureChildren(widthMeasureSpec, heightMeasureSpec);
+
+        int widthSize = MeasureSpec.getSize(widthMeasureSpec);
+        int widthSpec = MeasureSpec.getMode(widthMeasureSpec);
+        int heightSize = MeasureSpec.getSize(heightMeasureSpec);
+        int heightSpec = MeasureSpec.getMode(heightMeasureSpec);
+        if (childCount == 0) {
+            setMeasuredDimension(0, 0);
+        } else if (widthSpec == MeasureSpec.AT_MOST && heightSpec == MeasureSpec.AT_MOST) {
+            final View childView = getChildAt(0);
+            measuredWidth = childView.getMeasuredWidth() * childCount;
+            measuredHeight = childView.getMeasuredHeight();
+            setMeasuredDimension(measuredWidth, measuredHeight);
+        } else if (heightSpec == MeasureSpec.AT_MOST) {
+            final View childView = getChildAt(0);
+            measuredHeight = childView.getMeasuredHeight();
+            setMeasuredDimension(widthSize, childView.getMeasuredHeight());
+        } else if (widthSpec == MeasureSpec.AT_MOST) {
+            final View childView = getChildAt(0);
+            measuredWidth = childView.getMeasuredWidth() * childCount;
+            setMeasuredDimension(measuredWidth, heightSize);
+        }
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        int childTop = 0;
+        final int childCount = getChildCount();
+        mChildrenSize = childCount;
+
+        for (int i = 0; i < childCount; i++) {
+            final View childView = getChildAt(i);
+            if (childView.getVisibility() != View.GONE) {
+                final int childHeight = childView.getMeasuredHeight();
+                mChildHeight = childHeight;
+                childView.layout(0, childTop, childView.getMeasuredWidth(), childTop + childHeight);
+                // childView.layout(0, childTop, childTop + childHeight, childView.getMeasuredHeight());
+                childTop += childHeight;
+            }
+        }
+    }
+
+    @Override
+    public void computeScroll() {
+        if (mScroller.computeScrollOffset()) {
+            scrollTo(mScroller.getCurrX(), mScroller.getCurrY());
+            postInvalidate();
+        }
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        mVelocityTracker.recycle();
+        super.onDetachedFromWindow();
+    }
+
+}
