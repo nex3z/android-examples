2e2862e24928134345c62eceabd8633b6312382c
nex3z <litianxing9@gmail.com>
2017-01-13 22:29:26 +0800

    Add ReactNativePlayground.

---
+++ ReactNativePlayground/android/app/src/main/java/com/nex3z/examples/reactnativeplayground/MainActivity.java
@@ -0,0 +1,15 @@
+package com.nex3z.examples.reactnativeplayground;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "ReactNativePlayground";
+    }
+}
