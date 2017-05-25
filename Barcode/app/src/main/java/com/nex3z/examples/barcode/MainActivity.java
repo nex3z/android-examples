864286fd3b31ff7366a6eae462b8751c8ddb6c72
nex3z <litianxing9@gmail.com>
2017-05-25 21:23:10 +0800

    Update Barcode.

--- Barcode/app/src/main/java/com/nex3z/examples/barcode/MainActivity.java
+++ Barcode/app/src/main/java/com/nex3z/examples/barcode/MainActivity.java
@@ -1,11 +1,16 @@
 package com.nex3z.examples.barcode;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -13,17 +18,24 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.zxing.ResultPoint;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
+import com.journeyapps.barcodescanner.BarcodeCallback;
+import com.journeyapps.barcodescanner.BarcodeResult;
+import com.journeyapps.barcodescanner.DecoratedBarcodeView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
-
+    private static final int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 1;
     private static final int SCAN_REQUEST_ZXING_SCANNER = 1;
     private static final int SCAN_REQUEST_ZXING_ANDROID_EMBEDDED = 2;
     private static final String CLIP_LABEL = "scan_result";
 
     private TextView mTxtResult;
+    DecoratedBarcodeView mDbvScanner;
     private String mResult = "";
 
     @Override
@@ -65,7 +77,34 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        mDbvScanner = (DecoratedBarcodeView) findViewById(R.id.dbv_scanner);
+        mDbvScanner.setStatusText("");
+        mDbvScanner.decodeContinuous(new BarcodeCallback() {
+            @Override
+            public void barcodeResult(BarcodeResult result) {
+                mResult = result.getText();
+                mTxtResult.setText(mResult);
+            }
+
+            @Override
+            public void possibleResultPoints(List<ResultPoint> resultPoints) {}
+        });
+
         mTxtResult = (TextView) findViewById(R.id.txt_result);
+
+        requestPermission();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mDbvScanner.resume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mDbvScanner.pause();
     }
 
     @Override
@@ -86,4 +125,31 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private void requestPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            Log.v(LOG_TAG, "requestPermission(): Requesting.");
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA},
+                    MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+        } else {
+            Log.v(LOG_TAG, "requestPermission(): Permission was granted already.");
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_READ_CONTACTS: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.v(LOG_TAG, "onRequestPermissionsResult(): Permission granted");
+                } else {
+                    Toast.makeText(this, R.string.need_camera_permission, Toast.LENGTH_LONG).show();
+                }
+                return;
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
 }
