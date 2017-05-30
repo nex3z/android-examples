4fbcaf9031de3dfca0bde496d1b88659274a352e
nex3z <litianxing9@gmail.com>
2017-05-30 20:37:49 +0800

    Try data class.

--- TryKotlin/app/src/main/java/com/nex3z/examples/trykotlin/data/entity/DiscoveryMovieEntity.kt
+++ TryKotlin/app/src/main/java/com/nex3z/examples/trykotlin/data/entity/DiscoveryMovieEntity.kt
@@ -1,10 +1,9 @@
 package com.nex3z.examples.trykotlin.data.entity
 
-class DiscoveryMovieEntity {
-
-    var page: Int = 0
-    var results: List<MovieEntity>? = null
-    var total_results: Int = 0
-    var total_pages: Int = 0
-
+class DiscoveryMovieEntity(
+        var page: Int = 0,
+        var results: List<MovieEntity>? = null,
+        var total_results: Int = 0,
+        var total_pages: Int = 0
+) {
 }
