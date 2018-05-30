4667a585edca5a963120ec7fcbe58892bacf3201
nex3z <litianxing9@gmail.com>
2018-05-30 21:57:53 +0800

    Update ChangeDpi.

--- ChangeDpi/app/src/main/java/com/nex3z/examples/changedpi/MainActivity.java
+++ ChangeDpi/app/src/main/java/com/nex3z/examples/changedpi/MainActivity.java
@@ -1,10 +1,14 @@
 package com.nex3z.examples.changedpi;
 
 import android.content.Intent;
+import android.graphics.Point;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Display;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -34,6 +38,21 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        DisplayMetrics metrics = getResources().getDisplayMetrics();
+        float density = metrics.density;
+        int dpi = (int)(density * 160);
+        Point point = new Point();
+        Display display = getWindowManager().getDefaultDisplay();
+        display.getRealSize(point);
+        int widthPx = point.x;
+        int heightPx = point.y;
+        int widthDp = (int)(widthPx / density);
+        int heightDp = (int)(heightPx / density);
+
+        TextView tvInfo = findViewById(R.id.tv_info);
+        tvInfo.setText(String.format(getString(R.string.info), density, dpi, heightPx, widthPx,
+                heightDp, widthDp));
     }
 
 }
